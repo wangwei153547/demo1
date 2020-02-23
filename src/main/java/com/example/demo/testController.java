97c983a1bf470bb407507ef02a8810d396cc6734
wangwei26046 <1292598342@qq.com>
2020-02-23 16:40:56 +0800

    T1001:我的提交439

--- src/main/java/com/example/demo/testController.java
+++ src/main/java/com/example/demo/testController.java
@@ -12,6 +12,6 @@ public class testController {
     @RequestMapping(value =  "test",method={RequestMethod.GET} )
     public String test( ){
 
-        return "你好aeeattffaa";
+        return "你好aeea2ffaa";
     }
 }
